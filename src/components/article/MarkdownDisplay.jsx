16f16d02721e3a2002fa02df31854b55fb7e677e
Osman Semir <osmanshek@gmail.com>
2025-11-12 18:43:22 +0400

    Update syntax highlighter theme

--- src/components/article/MarkdownDisplay.jsx
+++ src/components/article/MarkdownDisplay.jsx
@@ -2,17 +2,14 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import {
-  atomDark,
-  gruvboxLight,
-  materialDark,
-  materialLight,
-  oneDark,
+  solarizedDarkAtom,
+  oneLight,
 } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { useTheme } from "../../hooks/useTheme";
 
 function MarkdownDisplay({ markdown }) {
   const { theme } = useTheme();
-  const style = theme === "light" ? materialLight : oneDark;
+  const style = theme === "light" ? oneLight : solarizedDarkAtom;
 
   const components = {
     code: ({ inline, className, children, ...props }) => {
@@ -26,7 +23,6 @@ function MarkdownDisplay({ markdown }) {
             language={match[1]}
             style={style}
             showLineNumbers="true"
-            // customStyle="bg-background"
           />
         );
       }
@@ -35,7 +31,7 @@ function MarkdownDisplay({ markdown }) {
   };
 
   return (
-    <div className="prose xl:prose-xl prose-pre:bg-[#FAFAFA] dark:prose-pre:bg-[#282C34] dark:prose-invert">
+    <div className="prose xl:prose-xl prose-pre:bg-[#FAFAFA] dark:prose-pre:bg-[#002B36] dark:prose-invert">
       <article className="mx-auto w-full   ">
         <Markdown remarkPlugins={[remarkGfm]} components={components}>
           {markdown}
